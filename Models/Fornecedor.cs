2bcc02edb6d884c7e08dbb08e3e6edb7800a2229
AllanUnser <allan3958@gmail.com>
2025-05-12 14:32:45 -0300

    Criado os arquivos para representar as tabelas Fornecedores e Usuarios no banco de dados

---
+++ Models/Fornecedor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsultaCnpj.Models
+{
+    //Representa a tabela "Fornecedores" no banco de daos
+    public class Fornecedor
+    {
+        //chave primária da tabela
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //Garante que o ID seja auto-incrementado
+        public int Id { get; set; }
+
+        [Required]
+        public string Cnpj {  get; set; }
+
+        public string RazaoSocial { get; set; }
+        public string Fantasia {  get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Municipio { get; set; }
+        public string UF { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaCnpj.Models
+{
+    //Representa a tabela "Usuairos" no banco de dados
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NomeUsuario { get; set; }
+        public string Senha { get; set; }
+    }
+}
