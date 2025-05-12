3effea501776844078f5a2af5129aa6e490bf529
AllanUnser <allan3958@gmail.com>
2025-05-12 12:19:26 -0300

    Criado Formulário de busca CNPJ com API

---
+++ Forms/FrmBuscaCnpj.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ConsultaCnpj.Forms.CadastroFornecedores
+{
+    partial class FrmBuscaCnpj
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btSair = new System.Windows.Forms.Button();
+            this.btContinuar = new System.Windows.Forms.Button();
+            this.tbCNPJ = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btSair
+            // 
+            this.btSair.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSair.Location = new System.Drawing.Point(169, 130);
+            this.btSair.Name = "btSair";
+            this.btSair.Size = new System.Drawing.Size(124, 46);
+            this.btSair.TabIndex = 10;
+            this.btSair.Text = "SAIR";
+            this.btSair.UseVisualStyleBackColor = true;
+            this.btSair.Click += new System.EventHandler(this.btSair_Click);
+            // 
+            // btContinuar
+            // 
+            this.btContinuar.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btContinuar.Location = new System.Drawing.Point(12, 130);
+            this.btContinuar.Name = "btContinuar";
+            this.btContinuar.Size = new System.Drawing.Size(124, 46);
+            this.btContinuar.TabIndex = 9;
+            this.btContinuar.Text = "CONTINUAR";
+            this.btContinuar.UseVisualStyleBackColor = true;
+            this.btContinuar.Click += new System.EventHandler(this.btContinuar_Click);
+            // 
+            // tbCNPJ
+            // 
+            this.tbCNPJ.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCNPJ.Location = new System.Drawing.Point(12, 82);
+            this.tbCNPJ.Name = "tbCNPJ";
+            this.tbCNPJ.Size = new System.Drawing.Size(281, 25);
+            this.tbCNPJ.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 19);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "CNPJ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 30);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Cadastro Fornecedores";
+            // 
+            // FrmBuscaCnpj
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 198);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btSair);
+            this.Controls.Add(this.btContinuar);
+            this.Controls.Add(this.tbCNPJ);
+            this.Controls.Add(this.label2);
+            this.Name = "FrmBuscaCnpj";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmBuscaCnpj";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSair;
+        private System.Windows.Forms.Button btContinuar;
+        private System.Windows.Forms.TextBox tbCNPJ;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
