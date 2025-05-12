16aad74c980b5fd5ce9ba8b8904489b8ca8da714
AllanUnser <allan3958@gmail.com>
2025-05-12 11:26:56 -0300

    Criado classe para uso de padrão AbstractFactory

---
+++ Factories/MySqlRepositoryFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsultaCnpj.Repositories;
+
+namespace ConsultaCnpj.Factories
+{
+    //Implementação concreta da fábrica de repositórios para o banco de dados MySQL
+    public class MySqlRepositoryFactory : IRepositoryFactory
+    {
+        //Retorna uma instância do repositório de Usuario específico para o MySQL
+        public IUsuarioRepository CriarUsuarioRepository()
+        {
+            //Retorna a implementação concreta de UsuarioRepository
+            return new UsuarioRepository();
+        }
+    }
+}
