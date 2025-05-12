1b3ebf911e16b8415a906a34e7efead13fafe607
AllanUnser <allan3958@gmail.com>
2025-05-12 11:19:29 -0300

    Criado interface para uso de padrão AbstractFactory

---
+++ Factories/IRepositoryFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsultaCnpj.Repositories;
+
+namespace ConsultaCnpj.Factories
+{
+    //Interface para Abstract Factory de repositórios
+    //Define contratos para obterrepositórios específicos, promovendo desacoplamento e flexibilidade
+    public interface IRepositoryFactory
+    {
+        //Retorna uma instância de repositório para a entidade Usuario
+        IUsuarioRepository CriarUsuarioRepository();
+
+    }
+
+}
