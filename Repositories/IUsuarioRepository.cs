694d58f556cdc780b58b607bc9191b9d89f6d8fa
AllanUnser <allan3958@gmail.com>
2025-05-12 16:10:24 -0300

    Criado a Interface do Usuario

---
+++ Repositories/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsultaCnpj.Models;
+
+namespace ConsultaCnpj.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        Usuario ObterUsuario(string nome, string senha);
+    }
+}
---
+++ Repositories/UsuarioRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsultaCnpj.Data;
+using ConsultaCnpj.Models;
+
+namespace ConsultaCnpj.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        public AppDbContext _context;
+
+        public UsuarioRepository()
+        {
+            _context = AppDbContext.GetInstance();
+        }
+
+        public Usuario ObterUsuario(string nome, string senha)
+        {
+            return _context.Usuarios.FirstOrDefault(u => u.NomeUsuario == nome && u.Senha == senha);
+        }
+    }
+}
