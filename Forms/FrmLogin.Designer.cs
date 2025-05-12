59a25ce5daf070ac7b3b004316a03cf5cc9edefd
AllanUnser <allan3958@gmail.com>
2025-05-12 14:11:52 -0300

    Criado Formulário de Login

---
+++ Forms/FrmLogin.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ConsultaCnpj.Forms.Login
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbUsuario = new System.Windows.Forms.TextBox();
+            this.tbSenha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btEntrar = new System.Windows.Forms.Button();
+            this.btSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuario";
+            // 
+            // tbUsuario
+            // 
+            this.tbUsuario.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUsuario.Location = new System.Drawing.Point(12, 103);
+            this.tbUsuario.Name = "tbUsuario";
+            this.tbUsuario.Size = new System.Drawing.Size(281, 26);
+            this.tbUsuario.TabIndex = 2;
+            // 
+            // tbSenha
+            // 
+            this.tbSenha.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSenha.Location = new System.Drawing.Point(12, 175);
+            this.tbSenha.Name = "tbSenha";
+            this.tbSenha.Size = new System.Drawing.Size(281, 26);
+            this.tbSenha.TabIndex = 4;
+            this.tbSenha.TextChanged += new System.EventHandler(this.tbSenha_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Senha";
+            // 
+            // btEntrar
+            // 
+            this.btEntrar.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEntrar.Location = new System.Drawing.Point(13, 228);
+            this.btEntrar.Name = "btEntrar";
+            this.btEntrar.Size = new System.Drawing.Size(124, 46);
+            this.btEntrar.TabIndex = 5;
+            this.btEntrar.Text = "ENTRAR";
+            this.btEntrar.UseVisualStyleBackColor = true;
+            this.btEntrar.Click += new System.EventHandler(this.btEntrar_Click);
+            // 
+            // btSair
+            // 
+            this.btSair.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btSair.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSair.Location = new System.Drawing.Point(169, 228);
+            this.btSair.Name = "btSair";
+            this.btSair.Size = new System.Drawing.Size(124, 46);
+            this.btSair.TabIndex = 6;
+            this.btSair.Text = "SAIR";
+            this.btSair.UseVisualStyleBackColor = true;
+            this.btSair.Click += new System.EventHandler(this.btSair_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AcceptButton = this.btEntrar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btSair;
+            this.ClientSize = new System.Drawing.Size(319, 286);
+            this.Controls.Add(this.btSair);
+            this.Controls.Add(this.btEntrar);
+            this.Controls.Add(this.tbSenha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbUsuario;
+        private System.Windows.Forms.TextBox tbSenha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btEntrar;
+        private System.Windows.Forms.Button btSair;
+    }
+}
---
+++ Forms/FrmLogin.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConsultaCnpj.Factories;
+using ConsultaCnpj.Forms.Principal;
+using ConsultaCnpj.Repositories;
+
+namespace ConsultaCnpj.Forms.Login
+{
+    public partial class FrmLogin : Form
+    {
+
+        //Construtor do formulário de Login
+        public FrmLogin()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void btEntrar_Click(object sender, EventArgs e)
+        {
+            //Criação da fábrica de usuários através da fábrica
+            var factory = new MySqlRepositoryFactory();
+
+            // Realiza a verificação de login no banco de dados
+            var usuarioRepo = factory.CriarUsuarioRepository();
+
+            //Realiza a verificação no banco de dados
+            var usuario = usuarioRepo.ObterUsuario(tbUsuario.Text, tbSenha.Text);
+
+            if (usuario != null)
+            {
+                //Abrir tela principal
+                FrmPrincipal telaPrincipal = new FrmPrincipal();
+                telaPrincipal.Show();
+
+                //Ocultar a tela de login
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuário ou Senha inválidos!");
+            }
+        }
+
+        private void btSair_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show(
+                "Deseja realmente sair ?",
+                "Sair",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+                );
+
+            if (resultado == DialogResult.Yes)
+            {
+                Application.Exit();
+            }    
+        }
+
+        private void tbSenha_TextChanged(object sender, EventArgs e)
+        {
+            //Ativa o modo de senha com caracteres ocultos
+            tbSenha.UseSystemPasswordChar = true;
+        }
+    }
+}
