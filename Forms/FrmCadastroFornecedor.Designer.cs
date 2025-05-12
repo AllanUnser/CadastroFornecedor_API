de6ceae0054faba340c2c3ca533bf5704f769868
AllanUnser <allan3958@gmail.com>
2025-05-12 13:43:59 -0300

    Criado Formulário Cadastro de Fornecedor

---
+++ Forms/FrmCadastroFornecedor.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace ConsultaCnpj.Forms.CadastroFornecedores
+{
+    partial class FrmCadastroFornecedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btSair = new System.Windows.Forms.Button();
+            this.tbRazaoSocial = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFantasia = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbCnpj = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbCep = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbLogradouro = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbBairro = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbMunicipio = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbUf = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbNumero = new System.Windows.Forms.TextBox();
+            this.tbTelefone = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(146, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 30);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Cadastro Fornecedores";
+            // 
+            // btSair
+            // 
+            this.btSair.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSair.Location = new System.Drawing.Point(318, 367);
+            this.btSair.Name = "btSair";
+            this.btSair.Size = new System.Drawing.Size(124, 46);
+            this.btSair.TabIndex = 14;
+            this.btSair.Text = "SAIR";
+            this.btSair.UseVisualStyleBackColor = true;
+            this.btSair.Click += new System.EventHandler(this.btSair_Click);
+            // 
+            // tbRazaoSocial
+            // 
+            this.tbRazaoSocial.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbRazaoSocial.Location = new System.Drawing.Point(16, 113);
+            this.tbRazaoSocial.Name = "tbRazaoSocial";
+            this.tbRazaoSocial.Size = new System.Drawing.Size(281, 25);
+            this.tbRazaoSocial.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 19);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nome/Razao Social";
+            // 
+            // tbFantasia
+            // 
+            this.tbFantasia.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFantasia.Location = new System.Drawing.Point(16, 174);
+            this.tbFantasia.Name = "tbFantasia";
+            this.tbFantasia.Size = new System.Drawing.Size(281, 25);
+            this.tbFantasia.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 19);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Nome Fantasia";
+            // 
+            // tbCnpj
+            // 
+            this.tbCnpj.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCnpj.Location = new System.Drawing.Point(16, 235);
+            this.tbCnpj.Name = "tbCnpj";
+            this.tbCnpj.Size = new System.Drawing.Size(281, 25);
+            this.tbCnpj.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 19);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "CNPJ";
+            // 
+            // tbCep
+            // 
+            this.tbCep.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCep.Location = new System.Drawing.Point(318, 113);
+            this.tbCep.Name = "tbCep";
+            this.tbCep.Size = new System.Drawing.Size(96, 25);
+            this.tbCep.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(314, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 19);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "CEP";
+            // 
+            // tbLogradouro
+            // 
+            this.tbLogradouro.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLogradouro.Location = new System.Drawing.Point(318, 174);
+            this.tbLogradouro.Name = "tbLogradouro";
+            this.tbLogradouro.Size = new System.Drawing.Size(222, 25);
+            this.tbLogradouro.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(314, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 19);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Logradouro";
+            // 
+            // tbBairro
+            // 
+            this.tbBairro.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbBairro.Location = new System.Drawing.Point(318, 235);
+            this.tbBairro.Name = "tbBairro";
+            this.tbBairro.Size = new System.Drawing.Size(295, 25);
+            this.tbBairro.TabIndex = 25;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(314, 213);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 19);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Bairro";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // tbMunicipio
+            // 
+            this.tbMunicipio.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbMunicipio.Location = new System.Drawing.Point(420, 113);
+            this.tbMunicipio.Name = "tbMunicipio";
+            this.tbMunicipio.Size = new System.Drawing.Size(120, 25);
+            this.tbMunicipio.TabIndex = 27;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(416, 91);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 19);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Cidade";
+            // 
+            // tbUf
+            // 
+            this.tbUf.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUf.Location = new System.Drawing.Point(547, 113);
+            this.tbUf.Name = "tbUf";
+            this.tbUf.Size = new System.Drawing.Size(66, 25);
+            this.tbUf.TabIndex = 29;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(543, 91);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 19);
+            this.label9.TabIndex = 28;
+            this.label9.Text = "UF";
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSalvar.Location = new System.Drawing.Point(173, 367);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(124, 46);
+            this.btSalvar.TabIndex = 30;
+            this.btSalvar.Text = "SALVAR";
+            this.btSalvar.UseVisualStyleBackColor = true;
+            this.btSalvar.Click += new System.EventHandler(this.btSalvar_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(543, 152);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 19);
+            this.label10.TabIndex = 31;
+            this.label10.Text = "Numero";
+            // 
+            // tbNumero
+            // 
+            this.tbNumero.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNumero.Location = new System.Drawing.Point(546, 174);
+            this.tbNumero.Name = "tbNumero";
+            this.tbNumero.Size = new System.Drawing.Size(67, 25);
+            this.tbNumero.TabIndex = 32;
+            // 
+            // tbTelefone
+            // 
+            this.tbTelefone.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTelefone.Location = new System.Drawing.Point(16, 298);
+            this.tbTelefone.Name = "tbTelefone";
+            this.tbTelefone.Size = new System.Drawing.Size(281, 25);
+            this.tbTelefone.TabIndex = 34;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(12, 276);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(74, 19);
+            this.label11.TabIndex = 33;
+            this.label11.Text = "Telefone";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEmail.Location = new System.Drawing.Point(318, 298);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(295, 25);
+            this.tbEmail.TabIndex = 36;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(314, 276);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(51, 19);
+            this.label12.TabIndex = 35;
+            this.label12.Text = "Email";
+            // 
+            // tbId
+            // 
+            this.tbId.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbId.Location = new System.Drawing.Point(16, 61);
+            this.tbId.Name = "tbId";
+            this.tbId.ReadOnly = true;
+            this.tbId.Size = new System.Drawing.Size(66, 25);
+            this.tbId.TabIndex = 38;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(12, 39);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(25, 19);
+            this.label13.TabIndex = 37;
+            this.label13.Text = "ID";
+            // 
+            // FrmCadastroFornecedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 425);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.tbTelefone);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.tbNumero);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btSalvar);
+            this.Controls.Add(this.tbUf);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tbMunicipio);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbBairro);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbLogradouro);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbCep);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbCnpj);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbFantasia);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btSair);
+            this.Controls.Add(this.tbRazaoSocial);
+            this.Controls.Add(this.label2);
+            this.Name = "FrmCadastroFornecedor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmCadastroFornecedor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btSair;
+        private System.Windows.Forms.TextBox tbRazaoSocial;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbFantasia;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbCnpj;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbCep;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbLogradouro;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbBairro;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbMunicipio;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbUf;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbNumero;
+        private System.Windows.Forms.TextBox tbTelefone;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.Label label13;
+    }
+}
---
+++ Forms/FrmCadastroFornecedor.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConsultaCnpj.Data;
+using ConsultaCnpj.Models;
+using ConsultaCnpj.Services;
+using Newtonsoft.Json.Linq;
+using ConsultaCnpj.Repositories;
+using System.Diagnostics.Eventing.Reader;
+
+namespace ConsultaCnpj.Forms.CadastroFornecedores
+{
+    public partial class FrmCadastroFornecedor : Form
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        
+        //Construtor para cadastro Manual
+        public FrmCadastroFornecedor()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            tbId.BackColor = SystemColors.Control;
+            //Instância Singleton do repositório
+            _fornecedorRepository = FornecedorRepository.GetInstance();
+
+            //Define o proximo Id
+            using (var context = new AppDbContext())
+            {
+                int proximoId = context.Fornecedores.Any()
+                    ? context.Fornecedores.Max(f => f.Id) + 1
+                    : 1;
+
+                tbId.Text = proximoId.ToString();
+            }
+        }
+
+        //Construtor com dados preenchidos via API
+        public FrmCadastroFornecedor(JObject dadosCNPJ) : this()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            tbId.BackColor = SystemColors.Control;
+            _fornecedorRepository = FornecedorRepository.GetInstance();
+
+            //Preenchimento automático dos campos com dados da API
+            tbRazaoSocial.Text = dadosCNPJ["nome"].ToString();
+            tbFantasia.Text = dadosCNPJ["fantasia"].ToString();
+            tbCnpj.Text = dadosCNPJ["cnpj"].ToString();
+            tbCep.Text = dadosCNPJ["cep"].ToString();
+            tbLogradouro.Text = dadosCNPJ["logradouro"].ToString();
+            tbBairro.Text = dadosCNPJ["bairro"].ToString();
+            tbMunicipio.Text = dadosCNPJ["municipio"].ToString();
+            tbUf.Text = dadosCNPJ["uf"].ToString();
+            tbNumero.Text = dadosCNPJ["numero"].ToString();
+            tbTelefone.Text = dadosCNPJ["telefone"].ToString() ;   
+            tbEmail.Text = dadosCNPJ["email"].ToString() ;
+
+            //Define o proximo Id
+            using (var context = new AppDbContext()) 
+            { 
+            int proximoId = context.Fornecedores.Any()
+                ? context.Fornecedores.Max(f => f.Id) + 1
+                : 1;
+
+            tbId.Text = proximoId.ToString();
+            }
+        }
+
+        //Evento de clique no botão Salvar
+        private void btSalvar_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show(
+                "Deseja salvar ?",
+                "Salvar",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+                );
+            if ( resultado == DialogResult.Yes) 
+            { 
+                try
+                {
+                    using (var context = new AppDbContext())
+                    {
+                        //Cria um novo objeto Fornecedor com os dados dos campos
+                        var fornecedor = new Fornecedor
+                        {
+                            Cnpj = tbCnpj.Text.Trim(),
+                            RazaoSocial = tbRazaoSocial.Text.Trim(),
+                            Fantasia = tbFantasia.Text.Trim(),
+                            Logradouro = tbLogradouro.Text.Trim(),
+                            Numero = tbNumero.Text.Trim(),
+                            Bairro = tbBairro.Text.Trim(),
+                            Municipio = tbMunicipio.Text.Trim(),
+                            UF = tbUf.Text.Trim(),
+                            Cep = tbCep.Text.Trim(),
+                            Telefone = tbTelefone.Text.Trim(),
+                            Email = tbEmail.Text.Trim()
+                        };
+
+                        //Salva os dados no Banco de dados
+                        context.Fornecedores.Add(fornecedor);
+                        context.SaveChanges();
+
+                        MessageBox.Show("Fornecedor salvo com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Close(); //Fecha o formuláio após salvar
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao salvar fornecedor: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+        
+
+        //Evento de clique no botão Sair
+        private void btSair_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show(
+                "Deseja realmente sair ?",
+                "Sair",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+                );
+
+            if (resultado == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
